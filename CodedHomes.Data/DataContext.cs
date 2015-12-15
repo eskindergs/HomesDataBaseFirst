bb951677c763cc90182e196732ef2328f5a68aae
eskindergs <eskindergs@gmail.com>
2015-12-15 14:12:54 -0500

    Commit the DataContect class settup connection string

---
+++ CodedHomes.Data/DataContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodedHomes.Models;
+using System.Data.Entity;
+using System.Data;
+using System.Configuration;
+//using CodedHomes.Data.Configuration;
+
+
+namespace CodedHomes.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Home> Homes { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public static string ConnectionStringName
+        {
+            get
+            {
+                if (ConfigurationManager.AppSettings["ConnectionStringName"] != null)
+                {
+                    return ConfigurationManager.AppSettings["ConnectionStringName"].ToString();
+                }
+
+                return "DefaultConnection";
+            }
+        }
+
+
+
+    }
+}
