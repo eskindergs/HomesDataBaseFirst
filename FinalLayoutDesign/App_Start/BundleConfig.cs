d039d3106b0979dd518edb985f7bb0144d9a458e
eskindergs <eskindergs@gmail.com>
2015-12-15 16:43:13 -0500

    Commit Bundle Config class configurations

--- FinalLayoutDesign/App_Start/BundleConfig.cs
+++ FinalLayoutDesign/App_Start/BundleConfig.cs
@@ -8,23 +8,43 @@ namespace FinalLayoutDesign
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/site").Include(
+                       "~/Scripts/lib/jquery-{version}.js"
+                       , "~/Scripts/lib/bootstrap.js"
+                       , "~/Scripts/lib/knockout-{version}.js"
+                       , "~/Scripts/lib/underscore.js"
+                       , "~/Scripts/lib/H5F.js"
+                       //, "~/Scripts/app/_mixins.js"
+                       //, "~/Scripts/app/homesDataService.js"
+                       //, "~/Scripts/app/validationUtility.js"
+                       //, "~/Scripts/app/offlineUtility.js"
+                      //, "~/Scripts/app/serializer.js"
+
+                       ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/bootstrap-responsive.css",
+                "~/Content/site.css"));
+
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+                        "~/Scripts/lib/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/lib/jquery.unobtrusive*",
+                        "~/Scripts/lib/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+           bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/lib/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
+           
+            
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
                         "~/Content/themes/base/jquery.ui.resizable.css",
