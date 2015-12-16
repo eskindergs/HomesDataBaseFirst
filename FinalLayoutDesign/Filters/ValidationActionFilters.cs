175760729565fa7bb1743e99e15f9e8484e1d6fd
eskindergs <eskindergs@gmail.com>
2015-12-16 12:57:28 -0500

    I added using reference in to the validation Action filters class

--- FinalLayoutDesign/Filters/ValidationActionFilters.cs
+++ FinalLayoutDesign/Filters/ValidationActionFilters.cs
@@ -1,11 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json.Linq;
 using System.Linq;
-using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
 
 namespace FinalLayoutDesign.Filters
 {
-    public class ValidationActionFilters
+    public class ValidationActionFilters : ActionFilterAttribute
     {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            var modelState = actionContext.ModelState;
+            if (!modelState.IsValid)
+            {
+                var errors = new JObject();
+                foreach (var Key in modelState.Keys)
+                {
+                    var state = modelState[Key];
+                    if (state.Errors.Any())
+                    {
+                        errors[Key] = state.Errors.First().ErrorMessage;
+                    }
+                }
+                actionContext.Response = actionContext.Request.CreateResponse<JObject>(HttpStatusCode.BadRequest, errors);
+            }
+            // base.OnActionExecuting(actionContext);
+        }
     }
 }
