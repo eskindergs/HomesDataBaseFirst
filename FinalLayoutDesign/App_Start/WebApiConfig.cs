c31a46385b530a0a58ceffb45a764c7d45bddccb
eskindergs <eskindergs@gmail.com>
2015-12-16 12:15:05 -0500

    i added custom rout template
    config.Routes.MapHttpRoute(
                   name: "API",
                   routeTemplate: "api/homes/{id}",
                   defaults: new
                   {
                       controller = "HomesAPI",
                       id = RouteParameter.Optional
                   }
               );

--- FinalLayoutDesign/App_Start/WebApiConfig.cs
+++ FinalLayoutDesign/App_Start/WebApiConfig.cs
@@ -10,6 +10,17 @@ namespace FinalLayoutDesign
         public static void Register(HttpConfiguration config)
         {
             config.Routes.MapHttpRoute(
+               name: "API",
+               routeTemplate: "api/homes/{id}",
+               defaults: new
+               {
+                   controller = "HomesAPI",
+                   id = RouteParameter.Optional
+               }
+           );
+
+
+            config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
